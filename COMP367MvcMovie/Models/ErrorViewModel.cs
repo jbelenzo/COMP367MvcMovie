8d54c56bc4933b8c779b6e914a4a9460b4e0573a
jbelenzo <jbelenzo@my.centennialcollege.ca>
2025-03-16 16:07:39 -0400

    Initial commit

---
+++ COMP367MvcMovie/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace COMP367MvcMovie.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
